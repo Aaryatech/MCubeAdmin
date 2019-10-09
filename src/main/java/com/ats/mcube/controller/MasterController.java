6a0a32b0d495735b5180097a9ba5b4420e2afa4c
lenovo <lenovo@lenovo-Lenovo-ideapad-330-15IKB>
2019-10-09 16:54:11 +0530

    //harsha

--- src/main/java/com/ats/mcube/controller/MasterController.java
+++ src/main/java/com/ats/mcube/controller/MasterController.java
@@ -7,14 +7,46 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
  
-
+@Controller
+@Scope("session")
 public class MasterController {
 	
+	
+	@RequestMapping(value = "/userList", method = RequestMethod.GET)
+	public ModelAndView userList(Locale locale, Model model,HttpSession session) {
+
+		 
+ 			ModelAndView mav = new ModelAndView("Master1/userList");
+	 
+	 
+	 
+		return mav;
+	}
+
+	@RequestMapping(value = "/addUser", method = RequestMethod.GET)
+	public ModelAndView addUser(HttpServletRequest request, HttpServletResponse response) {
+	 
+		HttpSession session = request.getSession();
+		ModelAndView mav = null;
+		 
+		try {
+			mav = new ModelAndView("Master/addUser");
+	 
+	 
+		} catch (Exception e) {
+			System.err.println("Exce in communication " + e.getMessage());
+			e.printStackTrace();
+		}
+		return mav;
+	}
+
 	
 	
 	@RequestMapping(value = "/addCustomer", method = RequestMethod.GET)
@@ -48,32 +80,5 @@ public class MasterController {
 	}
 
 	 
-	@RequestMapping(value = "/userList", method = RequestMethod.GET)
-	public ModelAndView userList(Locale locale, Model model,HttpSession session) {
-
-		 
- 			ModelAndView mav = new ModelAndView("Master1/userList");
-	 
-	 
-	 
-		return mav;
-	}
-
-	@RequestMapping(value = "/addUser", method = RequestMethod.GET)
-	public ModelAndView addUser(HttpServletRequest request, HttpServletResponse response) {
-	 
-		HttpSession session = request.getSession();
-		ModelAndView mav = null;
-		 
-		try {
-			mav = new ModelAndView("Master/addUser");
-	 
-	 
-		} catch (Exception e) {
-			System.err.println("Exce in communication " + e.getMessage());
-			e.printStackTrace();
-		}
-		return mav;
-	}
-
+	
 }
